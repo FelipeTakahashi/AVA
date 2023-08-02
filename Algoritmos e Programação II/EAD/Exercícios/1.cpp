52cd014cef2b9970c9991a74909c0d8d13688a9c
Felipe Jun Takahashi <131722952+FelipeTakahashi@users.noreply.github.com>
2023-08-02 09:35:22 -0400

    Create 1.cpp

---
+++ Algoritmos e Programação II/EAD/Exercícios/1.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main() {
+	int x, y;
+		std::printf("Digite os valores dos dois inteiros:");
+		std::scanf("%d %d", &x, &y);
+	int soma = x + y;
+		std::printf("%d", soma);
+	return 0;
+}
